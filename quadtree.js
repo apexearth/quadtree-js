e5c8de24781bb4907c3a987b942eb37a516153ac
apexearth <apexearth@gmail.com>
2015-04-19 18:09:32 -0700

    First commit
    Still in the middle of refactoring & cleanup after taking the code from
    a project where it was originally used.

---
+++ quadtree.js
@@ -0,0 +1,320 @@
+module.exports = Quadtree;
+function Quadtree(options) {
+    initialize.call(this, options);
+}
+
+Quadtree.prototype.add = add;
+Quadtree.prototype.remove = remove;
+Quadtree.prototype.check = check;
+Quadtree.prototype.update = update;
+
+
+function initialize(options) {
+    options = options || {};
+    this.isRoot = options.parent == null;
+    if (this.isRoot) {
+        this.root = this;
+        this.allChildSectors = [];
+        this.translate = options.translate || function (obj) {return {x: obj.x, y: obj.y}};
+        this.dimension = options.dimension || 1E5;
+        this.sectorObjectLimit = options.sectorObjectLimit || 16;
+        this.duplicateCoordinateSensitivity = options.duplicateCoordinateSensitivity || .00001;
+        configureAsParent.call(this);
+    } else {
+        this.root = options.parent.root;
+        this.parent = options.parent;
+        increaseSectorCount.call(this);
+        configureAsChild.call(this);
+        this.dimension = options.parent.childDimension;
+    }
+
+    this.left = options.left || 0;
+    this.top = options.top || 0;
+    this.childDimension = this.dimension / 2;
+    this.objectCount = 0;
+    this.sectorCount = 0;
+}
+
+/**
+ * A parent sector contains other sectors. It does not directly contain objects.
+ */
+function configureAsParent() {
+    this.tree = [];
+    this.children = [];
+    if (this.objects) {
+        var i = this.objects.length;
+        while (i--) {
+            var obj = this.objects.pop();
+            var coordinates = this.root.translate(obj);
+            getLowestSectorAtCoordinates.call(this, coordinates.x, coordinates.y, 0, true).add(obj);
+        }
+    }
+
+    var indexOf = this.root.allChildSectors.indexOf(this);
+    if (indexOf > -1) this.root.allChildSectors.splice(indexOf, 1);
+
+    this.isChildSector = false;
+    this.objects = null;
+    this.nearbyObjects = null;
+    this.nearbySectors = null;
+    this.far = null;
+}
+
+/**
+ * A child sector does not contain other sectors. It directly contains objects.
+ */
+function configureAsChild() {
+    if (this.parent.children.indexOf(this) == -1) {
+        this.parent.children.push(this);
+        // TODO: I once thought this was necessary. I don't currently understand why.
+        orderArray.call(this.parent.children);
+    }
+
+    if (this.root.allChildSectors.indexOf(this) === -1) {
+        this.root.allChildSectors.push(this);
+        // TODO: I once thought this was necessary. I don't currently understand why.
+        orderArray.call(this.root.allChildSectors);
+    }
+
+    this.isChildSector = true;
+    this.tree = null;
+    this.children = null;
+    this.objects = [];
+    this.nearbyObjects = [];
+    this.nearbySectors = [];
+    this.far = [];
+}
+
+function convertToParent() {
+    configureAsParent.call(this);
+    this.root.allChildSectors.splice(this.root.allChildSectors.indexOf(this), 1);
+}
+
+
+function update() {
+    updateNearby.call(this);
+    updateFar.call(this);
+}
+function updateNearby() {
+    this.nearbySectors = [];
+    this.nearbyObjects = [];
+    var x = this.left;
+    var y = this.top;
+
+    for (var ix = x - 1; ix <= x + 1; ix++) {
+        for (var iy = y - 1; iy <= y + 1; iy++) {
+            var temp = getLowestSectorAtCoordinates.call(this.root, ix * this.dimension, iy * this.dimension, this.dimension, false);
+            if (temp != null && this.nearbySectors.indexOf(temp) == -1) {
+                this.nearbySectors.push(temp);
+                var allObj = getAllObjects.call(temp);
+                var k = allObj.length;
+                while (k--) this.nearbyObjects.push(allObj[k]);
+            }
+        }
+    }
+}
+function updateFar() {
+    this.far = [];
+    var i = this.root.allChildSectors.length;
+    while (i--) {
+        var temp = this.root.allChildSectors[i];
+        if (this.nearbySectors.indexOf(temp) == -1) {
+            var j = this.nearbySectors.length, check = true;
+            while (j--) {
+                if (getAllChildren.call(this.nearbySectors[j]).indexOf(temp) >= 0) check = false;
+            }
+            if (check) this.far.push(temp);
+        }
+    }
+}
+
+
+/**
+ * Finds the lowest sector at the specified coordinate.
+ */
+function increaseSectorCount() {
+    var sector = this;
+    while (sector.parent != null) {
+        sector = sector.parent;
+        sector.sectorCount++;
+    }
+}
+function decreaseSectorCount() {
+    var sector = this;
+    while (sector.parent != null) {
+        sector.sectorCount--;
+        sector = sector.parent;
+    }
+}
+function getObjectSector(obj) {
+    var coordinate = this.root.translate(obj);
+    return getLowestSectorAtCoordinates.call(this, coordinate.x, coordinate.y, 0, false);
+}
+function getLowestSectorAtCoordinates(x, y, dimension, createIfNotFound) {
+    var sector = this;
+    var left;
+    var top;
+    while (sector.tree != null) {
+        left = Math.floor(x / sector.childDimension);
+        top = Math.floor(y / sector.childDimension);
+        var treeX = sector.tree[left];
+        if (!createIfNotFound && treeX == null) return null;
+        if (treeX == null) {
+            treeX = sector.tree[left] = [];
+            sector = treeX[top] = new Quadtree({parent: sector, left: left, top: top});
+        } else {
+            var treeY = treeX[top];
+            if (!createIfNotFound && treeY == null) return null;
+            sector = treeX[top] = (treeY != null ? treeY : new Quadtree({parent: sector, left: left, top: top}));
+        }
+
+        var countAtSameCoordinate = getObjectCountAtSameCoordinate.call(sector, {x: x, y: y});
+        if (countAtSameCoordinate < this.root.sectorObjectLimit && isSectorFull.call(sector) && sector.isChildSector) {
+            convertToParent.call(sector);
+        }
+
+
+        if (sector.dimension == dimension) break;
+    }
+    return sector;
+}
+
+/**
+ * This recursive method will return all child sectors underneath the current sector.
+ * @returns {Array}
+ */
+function getAllChildren() {
+    if (this.children == null) return [];
+    var i = this.children.length;
+    var array = [];
+    while (i--) {
+        var child = this.children[i];
+        array.push(child);
+        var childChildren = getAllChildren.call(child);
+        var k = childChildren.length;
+        while (k--) array.push(childChildren[k]);
+    }
+    return array;
+}
+/**
+ * This recursive method will return all objects in or underneath the current sector.
+ * @returns {Array}
+ */
+function getAllObjects() {
+    if (this.objects)
+        return this.objects;
+    else {
+        var i = this.children.length, array = [];
+        while (i--) {
+            var objects = getAllObjects.call(this.children[i]);
+            var j = objects.length - 1, k = -1;
+            while (k++ < j) array.push(objects[k]);
+        }
+        return array;
+    }
+}
+
+
+// Done Refactoring
+function orderArray() {
+    var i = this.length;
+    while (i-- > 1) {
+        var c1 = this[i];
+        var c2 = this[i - 1];
+        if (c1.left < c2.left || c1.left == c2.left && c1.top < c2.top) {
+            this.push(this.splice(i - 1, 1)[0]);
+        }
+    }
+}
+function isSectorFull() {
+    return this.objectCount >= this.root.sectorObjectLimit;
+}
+
+
+function add(obj) {
+    var coordinate = this.root.translate(obj);
+    var sector = getLowestSectorAtCoordinates.call(this, coordinate.x, coordinate.y, 0, true);
+    addObjectToSelf.call(sector);
+    addObjectToNearbyObjects.call(sector, obj);
+    addObjectToParents.call(sector);
+
+}
+function remove(obj) {
+    var sector = getObjectSector.call(this, obj);
+    removeObjectFromSelf.call(sector, obj);
+    removeObjectFromNearbySectors.call(sector, obj);
+    removeObjectFromParents.call(sector);
+}
+
+function addObjectToSelf(obj) {
+    this.objectCount++;
+    this.objects.push(obj);
+    this.nearbyObjects.push(obj);
+}
+function removeObjectFromSelf(obj) {
+    var objectIndex = this.objects.indexOf(obj);
+    this.objectCount--;
+    this.objects.splice(objectIndex, 1);
+
+    var nearbyObjectsIndex = this.nearbyObjects.indexOf(obj);
+    if (nearbyObjectsIndex !== -1) this.nearbyObjects.splice(nearbyObjectsIndex, 1);
+
+    if (this.objectCount == 0) {
+        var indexOf = this.root.allChildSectors.indexOf(this);
+        if (indexOf > -1) this.root.allChildSectors.splice(indexOf, 1);
+    }
+}
+
+function addObjectToParents() {
+    var parent = this.parent;
+    while (parent != null) {
+        parent.objectCount++;
+        parent = parent.parent;
+    }
+}
+function removeObjectFromParents() {
+    var parent = this.parent;
+    while (parent != null) {
+        parent.objectCount--;
+        if (parent.objectCount == 0 && parent.parent != null) {
+            decreaseSectorCount.call(parent);
+            configureAsChild.call(parent);
+        }
+        parent = parent.parent;
+    }
+}
+
+function addObjectToNearbyObjects(obj) {
+    var nearbySectorsIndex = this.nearbySectors.length;
+    var nearbySector;
+    while (nearbySectorsIndex--) {
+        nearbySector = this.nearbySectors[nearbySectorsIndex];
+        if (nearbySector.nearbyObjects != null)
+            nearbySector.nearbyObjects.push(obj);
+    }
+}
+function removeObjectFromNearbySectors(obj) {
+    var nearbySectorsIndex = this.nearbySectors.length;
+    var nearbySector;
+    var indexOf;
+    while (nearbySectorsIndex--) {
+        nearbySector = this.nearbySectors[nearbySectorsIndex];
+        if (nearbySector.nearbyObjects != null) {
+            indexOf = nearbySector.nearbyObjects.indexOf(obj);
+            if (indexOf > -1) nearbySector.nearbyObjects.splice(indexOf, 1);
+        }
+    }
+}
+
+
+function getObjectCountAtSameCoordinate(coordinate) {
+    var countAtSameCoordinate = 0;
+    var childrenIndex = this.objects.length;
+    while (childrenIndex--) {
+        var treeObject = this.objects[childrenIndex];
+        if (Math.abs(treeObject.x - coordinate.x) < this.root.duplicateCoordinateSensitivity &&
+            Math.abs(treeObject.y - coordinate.y) < this.root.duplicateCoordinateSensitivity)
+            countAtSameCoordinate++;
+    }
+    return countAtSameCoordinate;
+}
---
+++ tests/quadtree-tests.js
@@ -0,0 +1,42 @@
+var should = require('should');
+
+var Quadtree = require('../quadtree');
+
+describe('Quadtree Tests', function () {
+
+    it('does stuff', function () {
+        var quadtree = new Quadtree({
+            dimension: 10,
+            sectorObjectLimit: 1
+        });
+        quadtree.sectorCount.should.equal(0);
+
+        var obj1 = {x: 5, y: 5, radius: 1};
+        quadtree.add(obj1);
+        quadtree.objectCount.should.equal(1);
+
+        var obj2 = {x: 5, y: 5, radius: 1};
+        quadtree.add(obj2);
+        quadtree.objectCount.should.equal(2);
+        quadtree.sectorCount.should.equal(1);
+
+        var intersections = quadtree.check(obj2);
+        intersectoins.length.should.equal(1);
+        intersections[0].object.should.equal(obj1);
+
+        var obj3 = {x: -5, y: -5, radius: 1};
+        quadtree.add(obj3);
+        quadtree.objectCount.should.equal(3);
+        quadtree.sectorCount.should.equal(2);
+
+        var obj4 = {x: 4, y: 4, radius: 1};
+        quadtree.add(obj4);
+        quadtree.objectCount.should.equal(4);
+        quadtree.sectorCount.should.equal(3);
+
+        quadtree.remove(obj2);
+        quadtree.objectCount.should.equal(3);
+        quadtree.sectorCount.should.equal(3);
+    })
+
+});
